8c9047967beac4184aad8a187c301ddcef5984b3
Vladimir Fibe <motiw@icloud.com>
2023-08-22 11:58:26 +0600

    Finish

--- FoodDeliveryLayout/FoodDeliveryLayout/FoodController.swift
+++ FoodDeliveryLayout/FoodDeliveryLayout/FoodController.swift
@@ -4,28 +4,103 @@ class FoodController: UICollectionViewController {
     let cellId = "cellId"
     init() {
         let layout = UICollectionViewCompositionalLayout { sectionNumber, _ in
-            let item = NSCollectionLayoutItem(layoutSize: .init(
-                widthDimension: .fractionalWidth(1),
-                heightDimension: .fractionalHeight(1)))
-            item.contentInsets.bottom = 16
-
-            let group = NSCollectionLayoutGroup.horizontal(
-                layoutSize: .init(
+            switch sectionNumber {
+            case 0:
+                let item = NSCollectionLayoutItem(layoutSize: .init(
                     widthDimension: .fractionalWidth(1),
-                    heightDimension: .absolute(300)),
-                subitems: [item])
+                    heightDimension: .fractionalHeight(1)))
+                item.contentInsets.trailing = 2
 
-            let section = NSCollectionLayoutSection(group: group)
-            return section
+                let group = NSCollectionLayoutGroup.horizontal(
+                    layoutSize: .init(
+                        widthDimension: .fractionalWidth(1),
+                        heightDimension: .absolute(200)),
+                    subitems: [item])
+
+                let section = NSCollectionLayoutSection(group: group)
+                section.orthogonalScrollingBehavior = .paging
+                return section
+            case 1:
+                let item = NSCollectionLayoutItem(layoutSize: .init(
+                    widthDimension: .fractionalWidth(0.25),
+                    heightDimension: .absolute(150)))
+                item.contentInsets.trailing = 16
+                item.contentInsets.bottom = 16
+
+                let group = NSCollectionLayoutGroup.horizontal(
+                    layoutSize: .init(
+                        widthDimension: .fractionalWidth(1),
+                        heightDimension: .estimated(500)),
+                    subitems: [item])
+
+                let section = NSCollectionLayoutSection(group: group)
+                section.contentInsets.leading = 16
+                section.boundarySupplementaryItems = [
+                    .init(layoutSize: .init(
+                        widthDimension: .fractionalWidth(0.75),
+                        heightDimension: .absolute(50)),
+                    elementKind: "categoryHeaderId",
+                          alignment: .topLeading)
+                ]
+                return section
+            case 2:
+                let item = NSCollectionLayoutItem(layoutSize: .init(
+                    widthDimension: .fractionalWidth(1),
+                    heightDimension: .fractionalHeight(1)))
+                item.contentInsets.trailing = 16
+                item.contentInsets.bottom = 16
+                let group = NSCollectionLayoutGroup.horizontal(
+                    layoutSize: .init(
+                        widthDimension: .fractionalWidth(0.8),
+                        heightDimension: .absolute(125)),
+                    subitems: [item])
+
+                let section = NSCollectionLayoutSection(group: group)
+                section.orthogonalScrollingBehavior = .continuous
+                section.contentInsets.leading = 16
+                return section
+            default:
+                let item = NSCollectionLayoutItem(layoutSize: .init(
+                    widthDimension: .fractionalWidth(0.5),
+                    heightDimension: .absolute(300)))
+                item.contentInsets.trailing = 16
+                item.contentInsets.bottom = 16
+                let group = NSCollectionLayoutGroup.horizontal(
+                    layoutSize: .init(
+                        widthDimension: .fractionalWidth(1),
+                        heightDimension: .estimated(500)),
+                    subitems: [item])
+
+                let section = NSCollectionLayoutSection(group: group)
+                section.contentInsets.leading = 16
+                return section
+            }
         }
 
         super.init(collectionViewLayout: layout)
     }
 
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let controller = UIViewController()
+        let colors = [UIColor.red, .yellow, .green, .systemPink, .cyan, .orange]
+        controller.view.backgroundColor = colors[indexPath.section]
+        navigationController?.pushViewController(controller, animated: true)
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(
+            ofKind: kind,
+            withReuseIdentifier: "headerId",
+            for: indexPath)
+        return header
+    }
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        4
+    }
     override func collectionView(
         _ collectionView: UICollectionView,
         numberOfItemsInSection section: Int) -> Int {
-        3
+        8
     }
 
     override func collectionView(
@@ -47,10 +122,29 @@ class FoodController: UICollectionViewController {
         collectionView.register(
             UICollectionViewCell.self,
             forCellWithReuseIdentifier: cellId)
+        collectionView.register(HeaderView.self,
+                                forSupplementaryViewOfKind: "categoryHeaderId",
+                                withReuseIdentifier: "headerId")
     }
 }
 
+class HeaderView: UICollectionReusableView {
+    let label = UILabel()
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        label.text = "Categories"
+        addSubview(label)
+    }
 
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        label.frame = bounds
+    }
+}
 
 
 
--- FoodDeliveryLayout/README.md
+++ FoodDeliveryLayout/README.md
@@ -1,4 +1,4 @@
-#  Food Delivery Layout
+#  [Food Delivery Layout](https://youtu.be/y1uXXVUu43o)
 
 ![](img/01.png)
 ![](img/02.png)
