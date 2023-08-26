e85e79b4e4420edfa226f5465993061771efd710
Vladimir Fibe <motiw@icloud.com>
2023-08-26 14:55:28 +0600

    AdvancedLayouts

---
+++ AdvancedLayouts/AdvancedLayouts/PhotoCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoCollectionViewCell.swift
+//  AdvancedLayouts
+//
+//  Created by Vladimir Fibe on 24.08.2023.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+}
